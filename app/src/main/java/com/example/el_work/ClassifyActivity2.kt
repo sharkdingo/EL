c6cc22f46011838042761c2898dcd30ecf47515e
araraki koyomin <3099146950@qq.com>
2024-05-30 23:10:45 +0800

    Add paint part

--- app/src/main/java/com/example/el_work/ClassifyActivity2.kt
+++ app/src/main/java/com/example/el_work/ClassifyActivity2.kt
@@ -19,12 +19,28 @@ import androidx.core.view.WindowInsetsCompat
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-
+import android.content.Intent
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.drawable.ColorDrawable
+import android.view.MotionEvent
+import android.view.View
 class ClassifyActivity2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    private val colors = mutableListOf<Int>()
+    private lateinit var imageView: ImageView
+    private lateinit var canvas: Canvas
+    private lateinit var paint:Paint
     private lateinit var bitmap: Bitmap
     private lateinit var dbHelper: DatabaseHelper
 
-    @SuppressLint("MissingInflatedId")
+    private var startX = 0f
+    private var startY = 0f
+
+
+    @SuppressLint("ClickableViewAccessibility", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -39,6 +55,86 @@ class ClassifyActivity2 : AppCompatActivity() {
         val saveButton: Button = findViewById(R.id.button9)
         saveButton.setOnClickListener{
             saveBitmap()
+        }
+
+        val button1 = findViewById<Button>(R.id.button6)
+        val button2 = findViewById<Button>(R.id.button7)
+        val button3 = findViewById<Button>(R.id.button8)
+        val button4 = findViewById<Button>(R.id.button10)
+        val button5 = findViewById<Button>(R.id.button)
+        var initialButtonColor = button4.backgroundTintList?.defaultColor ?: Color.BLACK
+
+        imageView = findViewById(R.id.imageView5)
+        val originalBitmap = BitmapFactory.decodeResource(resources, R.drawable.hehuatest)
+        bitmap = Bitmap.createBitmap(originalBitmap.width, originalBitmap.height, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(bitmap)
+        canvas.drawBitmap(originalBitmap, 0f, 0f, null)
+        imageView.setImageBitmap(bitmap)
+
+        imageView.setOnTouchListener { v, event ->
+            val scaledX = event.x / v.width * bitmap.width
+            val scaledY = event.y / v.height * bitmap.height
+
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    startX = scaledX
+                    startY = scaledY
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    canvas.drawLine(startX, startY, scaledX, scaledY, paint)
+                    imageView.invalidate()
+                    startX = scaledX
+                    startY = scaledY
+                }
+            }
+            true
+        }
+        paint = Paint().apply {
+            color = initialButtonColor
+            strokeWidth = 45f
+            style = Paint.Style.STROKE
+        }
+
+        button1.setOnClickListener {
+            val defaultColor = Color.parseColor("#FFFFFFFF")
+            val currentColor = button1.backgroundTintList?.getColorForState(button1.drawableState, defaultColor)
+            if (currentColor != null) {
+                colors.add(currentColor)
+            }
+            paint.color=Color.rgb(255,0,0)
+            button1.invalidate()
+
+        }
+
+        button2.setOnClickListener {
+            val defaultColor = Color.parseColor("#FFFFFFFF")
+            val currentColor = button2.backgroundTintList?.getColorForState(button2.drawableState, defaultColor)
+            if (currentColor != null) {
+                colors.add(currentColor)
+            }
+            paint.color=Color.rgb(0,255,0)
+            button2.invalidate()
+        }
+
+        button3.setOnClickListener {
+            val defaultColor = Color.parseColor("#FFFFFFFF")
+            val currentColor = button3.backgroundTintList?.getColorForState(button3.drawableState, defaultColor)
+            if (currentColor != null) {
+                colors.add(currentColor)
+            }
+
+            paint.color=Color.rgb(0,0,255)
+            button3.invalidate()
+        }
+        button5.setOnClickListener {
+            val defaultColor = Color.parseColor("#FFFFFFFF")
+            val currentColor = button3.backgroundTintList?.getColorForState(button3.drawableState, defaultColor)
+            if (currentColor != null) {
+                colors.add(currentColor)
+            }
+
+            paint.color=Color.rgb(85,85,85)
+            button5.invalidate()
         }
     }
 
@@ -81,6 +177,11 @@ class ClassifyActivity2 : AppCompatActivity() {
         }
         db.insert("drawings", null, values)
         println(path)
+    }
+
+    fun goToActivity2(view: View) {
+        val intent = Intent(this, ClassifyActivity2::class.java)
+        startActivity(intent)
     }
 
 }
